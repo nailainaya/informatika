ec368c78061028ea83509cadad370f1ed6afa4ac
nailainaya <146616656+nailainaya@users.noreply.github.com>
2024-08-27 09:27:04 +0700

    Add files via upload

---
+++ tugas e lerning infor 1.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print("Belajar Python dari Nol")
+
+judul = "Belajar Pemrograman Python sampai Bisa"
+print(judul)
+
+## Snake Case digunakan pada:module_name, package_name, method_name, function_name , global_var_name,instance_var_name, function_parameter_name, local_var_name.
+## Camel Case digunakan Pada: ClassName, ExceptionName
+## Kebab Case digunakan pada: btn-primary
+## ALL CAPS digunakan Pada: GLOBAL_CONSTANT_NAME
+
+username = "pyhtonpemula"
+
+if username == 'pythonpemula':
+    print("Selamat Datang Admin")
+    print("Silakan ambil tempat duduk")
+    
+variabel_ku = "ini isi variabel"
+variabel2 = 20 
+
+#_nama, naya, nama_variabel
+
